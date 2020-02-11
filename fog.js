31e21aa1c467e0659b9ec3f5bdb31beef0c9a015
Brian Brown II <bkeithbii@gmail.com>
2020-02-11 15:15:52 -0500

    defined variables in JS

--- fog.js
+++ fog.js
@@ -1,10 +1,17 @@
 //Create an array of colors
-let colors = ["blue", "silver", "white", "red"];
+let colors = ["blue", "purple", "yellow", "silver"];
 
-//Assign a variable to the game tiles
+//Assign variables to different elements in game (tile, level, active, start/quit/instruction buttons)
 let tiles = document.querySelectorAll(".tile");
+let active = document.querySelectorAll(".active");
+let level = document.querySelector(".scoreLog");
+let startBtn = document.querySelector(".startButt");
+let quitBtn = document.querySelector(".quitButt");
+let instructions = document.querySelector(".instructions");
 
 //Assign a color to each game tile with for loop
 for (i = 0; i < colors.length; i++) {
   tiles[i].style.backgroundColor = colors[i];
 }
+
+//Assign active color to each game tile with for loop
