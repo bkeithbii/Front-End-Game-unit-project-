1dd606af149de3432e9ce59b8b3278b8e1c64712
Brian Brown II <bkeithbii@gmail.com>
2020-02-11 13:41:17 -0500

    assinged colors

---
+++ fog.js
@@ -0,0 +1,10 @@
+//Create an array of colors
+let colors = ["blue", "red", "silver", "gold"];
+
+//Assign a variable to the game tiles
+let square = document.querySelectorAll(".tiles");
+
+//Assign a color to each game tile with for loop
+for (i = 0; i < colors.length; i++) {
+  square[i].style.backgroundColor = colors[i];
+}
