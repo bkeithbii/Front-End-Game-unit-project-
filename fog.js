d5111a6043005b1337eaca9c7ae5d13ba654aa70
Brian Brown II <bkeithbii@gmail.com>
2020-02-11 14:47:39 -0500

    + active class to tiles

--- fog.js
+++ fog.js
@@ -2,9 +2,9 @@
 let colors = ["blue", "silver", "white", "red"];
 
 //Assign a variable to the game tiles
-let square = document.querySelectorAll(".tiles");
+let tiles = document.querySelectorAll(".tile");
 
 //Assign a color to each game tile with for loop
 for (i = 0; i < colors.length; i++) {
-  square[i].style.backgroundColor = colors[i];
+  tiles[i].style.backgroundColor = colors[i];
 }
