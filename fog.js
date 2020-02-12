4721ed8742264684d43c639b7a818f1ef0a4b5e2
Brian Brown II <bkeithbii@gmail.com>
2020-02-12 10:03:57 -0500

    redoing JS as well as some basic design, starting from scratch

--- fog.js
+++ fog.js
@@ -1,64 +1,70 @@
-//Create an array of colors
-let colors = ["blue", "purple", "yellow", "silver"];
+// //Create an array of colors
+// let colors = ["blue", "purple", "yellow", "silver"];
 
-//Assign variables to different elements in game (tile, level, active, start/quit/instruction buttons)
-let tiles = document.querySelectorAll(".tile");
-let active = document.querySelectorAll(".active");
-let level = document.querySelector(".scoreLog");
-let startBtn = document.querySelector(".startButt");
-let quitBtn = document.querySelector(".quitButt");
-let instructions = document.querySelector(".instructions");
+// //Assign variables to different elements in game (tile, level, active, start/quit/instruction buttons)
+// let tiles = document.querySelectorAll(".tile");
+// let active = document.querySelectorAll(".active");
+// let level = document.querySelector(".scoreLog");
+// let startBtn = document.querySelector(".startButt");
+// let quitBtn = document.querySelector(".quitButt");
+// let instructions = document.querySelector(".instructions");
 
-//Assign a color to each game tile with for loop
-for (i = 0; i < colors.length; i++) {
-  tiles[i].style.backgroundColor = colors[i];
-}
+// //Assign a color to each game tile with for loop
+// for (i = 0; i < colors.length; i++) {
+//   tiles[i].style.backgroundColor = colors[i];
+// }
 
-//Create an array for computer sequence (simon) and user sequence
-let cpuSeq = [];
-let userSeq = [];
+// //Create an array for computer sequence (simon) and user sequence
+// let cpuSeq = [];
+// let userSeq = [];
 
-//Assign level variable a starting value
-level.innerHTML = "Level:" + " " + 0;
+// //Assign level variable a starting value
+// level.innerHTML = 0;
 
-//Assign id a starting value
-id = 0;
+// //Assign id a starting value
+// // id = 0;
 
-//Begin creating functionality for start button
-startBtn.addEventListener("click", startGame);
+// //Begin creating functionality for start button
+// $("document").ready(function() {
+//   startBtn.addEventListener("click", startGame);
+// });
 
-//Define start game function
-function startGame() {
-  level.innerHTML++;
-  startSeq();
-}
+// //Define start game function
+// function startGame() {
+//   level.innerHTML++;
+//   startSeq();
+// }
 
-//Define start sequence function (computer sequence)
-function startSeq() {
-  console.log(level.innerHTML);
-  randomColor();
-  let i = 0;
-  let myInt = setInterval(interval, 1000);
-  addClass(id, color);
-}
+// //Define start sequence function (computer sequence)
+// function startSeq() {
+//   console.log(level.innerHTML);
+//   randomColor();
+//   let i = 0;
+//   let myInt = setInterval(interval, 1000);
+// }
 
-//Define function to generate random color selection
-function randomColor() {
-  let random = Math.floor(Math.random() * 4);
-  cpuSeq.push(random);
-}
+// //Define function to generate random color selection
+// function randomColor() {
+//   let random = Math.floor(Math.random() * 4);
+//   cpuSeq.push(random);
+// }
 
-//Define a set interval function for showing the tile color being pressed
-function interval() {
-  id = cpuSeq[i];
-  color = tiles[i];
-  console.log(id + " " + color);
-}
+// //Define a set interval function for showing the tile color being pressed
+// function interval() {
+//   id = cpuSeq[i];
+//   color = tiles[i];
+//   console.log(id + " " + color);
+//   addClass(id, color);
+//   i++;
+//   if (i === cpuSeq.length) {
+//     clearInterval(myInt);
+//   }
+// }
 
-//Define a function to add the active class to each tile during gameplay
-function addClass(id, color) {
-  id.classList.add(color + "active");
-  setTimeout(function() {
-    id.classList.remove(color + "active");
-  }, 500);
-}
+// //Define a function to add the active class to each tile during gameplay
+// function addClass(id, color) {
+//   $("#" + id).classList.add(color + "-active");
+//   setTimeout(function() {
+//     $("#" + id).classList.remove(color + "-active");
+//   }, 500);
+// }
